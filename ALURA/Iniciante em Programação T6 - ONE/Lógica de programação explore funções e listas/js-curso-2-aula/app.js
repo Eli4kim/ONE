e51c89a4e00cb3e2d8e16a900c0a4d32837e3a92
Eliakim A Araujo <eli4kim@gmail.com>
2024-01-25 15:42:45 -0300

    Update app.js

--- ALURA/Iniciante em Programação T6 - ONE/Lógica de programação explore funções e listas/js-curso-2-aula/app.js
+++ ALURA/Iniciante em Programação T6 - ONE/Lógica de programação explore funções e listas/js-curso-2-aula/app.js
@@ -4,13 +4,15 @@
 //paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
 
 // Lembrando que a forma acima , já feita antes, não esta errada porém refatora-se o codigo para manter boas praticas e mais limpo como no exemplo abaixo.
-
+let listaDeNumerosSorteados = [];
+let numeroLimite = 10;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
+    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2} );
 }
 
 function exibirMensagemInicial() { 
@@ -53,7 +55,21 @@ function verificarChute() {
 //O tipo booleano é um nome muito comum para outras linguagens de programação também. É um valor que pode ser verdadeiro ou falso.
 
 function gerarNumeroAleatorio() {
-   return parseInt(Math.random() * 10 + 1);
+    let numeroEscolhido =  parseInt(Math.random() * numeroLimite + 1);
+    let quantidadeDeElementosNaLista = listaDeNumerosSorteados.length;
+
+    if (quantidadeDeElementosNaLista == numeroLimite) {
+        listaDeNumerosSorteados = [];
+    }
+
+    if (listaDeNumerosSorteados.includes(numeroEscolhido)) {
+        return gerarNumeroAleatorio();
+    } else {
+        listaDeNumerosSorteados.push(numeroEscolhido);
+        console.log(listaDeNumerosSorteados);
+        return numeroEscolhido;
+
+    }
 }
 
     function limparCampo() {
