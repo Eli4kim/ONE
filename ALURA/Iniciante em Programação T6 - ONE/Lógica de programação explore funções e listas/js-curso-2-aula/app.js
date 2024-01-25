2fe6828c09587dd8eccb6054832872a476bd187b
Eliakim A Araujo <eli4kim@gmail.com>
2024-01-25 12:00:59 -0300

    Aula
    Desenvolvimento de aplicação , continuação.

--- ALURA/Iniciante em Programação T6 - ONE/Lógica de programação explore funções e listas/js-curso-2-aula/app.js
+++ ALURA/Iniciante em Programação T6 - ONE/Lógica de programação explore funções e listas/js-curso-2-aula/app.js
@@ -6,22 +6,65 @@
 // Lembrando que a forma acima , já feita antes, não esta errada porém refatora-se o codigo para manter boas praticas e mais limpo como no exemplo abaixo.
 
 let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
 
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
 }
 
-exibirTextoNaTela('h1', 'Jogo do número secreto');
-exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+function exibirMensagemInicial() { 
+    exibirTextoNaTela('h1', 'Jogo do número secreto'); 
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10'); I
+}
+
+exibirMensagemInicial();
+//exibirTextoNaTela('h1', 'Jogo do número secreto');
+//exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
 
 function verificarChute() {
     //corpor da função
     let chute = document.querySelector('input').value;
-    console.log(chute == numeroSecreto);
+    
+    if (chute == numeroSecreto) {
+        exibirTextoNaTela('h1', 'Acertou!');
+        let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+        let mensagemTentativas = `Você descobriu o número secreto como ${tentativas} ${palavraTentativa}!`;
+                exibirTextoNaTela('p', mensagemTentativas);
+                document.getElementById('reiniciar').removeAttribute('disabled');
+        } 
+        else {
+                if (chute > numeroSecreto) {
+                        exibirTextoNaTela('p', 'O número secreto é menor');
+                } else {
+                        exibirTextoNaTela('p', 'O número secreto é maior');
+                }
+                //tentativa = tentativas +1;
+                tentativas++;
+                limparCampo();
+        }
 }
+
+//function verificarChute() {
+    //corpor da função
+  //  let chute = document.querySelector('input').value;
+ //   console.log(chute == numeroSecreto);
+//}
 //O tipo booleano é um nome muito comum para outras linguagens de programação também. É um valor que pode ser verdadeiro ou falso.
 
 function gerarNumeroAleatorio() {
    return parseInt(Math.random() * 10 + 1);
 }
+
+    function limparCampo() {
+        chute = document.querySelector('input');
+        chute.value = '';
+    }
+
+        function reiniciarJogo() {
+            numeroSecreto = gerarNumeroAleatorio();
+            limparCampo();
+            tentativas = 1;
+            exibirMensagemInicial();
+            document.getElementById('reiniciar').setAttribute('disabled', true)
+        }
